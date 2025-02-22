072d3ca1e416e966124eefe1ffc6b1267a5481ff
yuming <zhaoyuming@corp.netease.com>
2025-02-22 19:01:48 +0800

    feat: 升级库版本，gradle 改为kts

---
+++ shared/src/commonMain/kotlin/com/youdao/kmp/MiniProgramType.kt
@@ -0,0 +1,21 @@
+package com.youdao.kmp
+
+enum class MiniProgramType {
+    /**
+     * 正式版
+     *
+     */
+    Release,
+
+    /**
+     * 测试版
+     *
+     */
+    Test,
+
+    /**
+     * 体验版
+     *
+     */
+    Preview
+}
